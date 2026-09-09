ceeacde0ad12793a9122f009fd2235fef77d0024
Pawel Walicki <pawel.walicki89@interia.pl>
2026-09-09 12:16:58 +0200

    corrected code in the component

--- src/app/components/CountrySelect.js
+++ src/app/components/CountrySelect.js
@@ -1,14 +1,21 @@
 import Select from "react-select";
 
-export function CountrySelect({options, setCountryFilter} ) {
-   
-    const countrySelected = (list) => {
-        setCountryFilter(list.map((e) => e.value))
-    }
+export function CountrySelect({ options, setCountryFilter }) {
+
+    const handleChange = (list) => {
+        const countries = list ? list.map((e) => e.value) : [];
+
+        setCountryFilter(countries);
+    };
+
 
     return (
-        <Select className="w-md" onChange = {(e) => countrySelected(e)}  isMulti options={options} >
-           
+        <Select className="w-md placeholder: () => text-slate-400"
+            onChange={handleChange}
+            isMulti
+            options={options} 
+            placeholder="Change countries...">
+
         </Select>
     )
 }
